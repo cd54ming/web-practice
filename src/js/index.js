5f5328490a1708ee7c9ee555f094a83b52a08782
ChinMing Kuo <theseus013059@gmail.com>
2019-09-17 10:43:48 +0800

    fixed some typo.

--- src/js/index.js
+++ src/js/index.js
@@ -37,7 +37,7 @@ const practiceData = [{
 }, {
   name: 'Read Clipboard',
   description: '讀取使用者的 Clipboard 內容',
-  link: 'readClipBoard',
+  link: 'readClipboard',
 }];
 
 window.onload = function instertData() {
