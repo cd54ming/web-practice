ba3ff90224d9840d7eadfc085fbe640b4c61c5bb
Kuo Chin Ming <hi54ming@gmail.com>
2019-09-23 16:41:03 +0800

    add placeholder image. modify index page title.

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,50 @@
 let practiceData = [{
-  name: '學學廣告DM',
-  description: 'CSS 排版',
+  name: '學學廣告 DM',
+  description: '練習 CSS position 排版。',
   link: 'XueXueDM',
 }, {
   name: 'Pantone 年度代表色',
-  description: 'CSS flex-box 史元素置中',
+  description: '使用 CSS flex-box 使元素水平、垂直置中。',
   link: 'pantoneColorOfTheYear',
 }, {
   name: '新垣結依照片牆',
-  description: 'CSS grid-box 排版',
+  description: '模仿 GMO 證券廣告頁面，練習 CSS grid-box 排版。',
   link: 'gakkiWall',
 }, {
   name: 'Grid Box Responsive Layout Practice',
-  description: 'CSS Grid Box 響應式排版練習',
+  description: '練習 CSS grid-box 響應式排版，縮小視窗寬度檢視效果。',
   link: 'gridResponsiveLayout/dist',
 }, {
   name: 'Remember To Breathe',
-  description: '全螢幕影片背景',
+  description: '實作全螢幕影片背景效果。',
   link: 'rememberToBreathe',
 }, {
   name: '彈跳球',
-  description: 'JavaScript class 練習',
+  description: '練習 JavaScript class。',
   link: 'bouncingBall',
 }, {
   name: 'Super Hero Squad',
-  description: '練習使用 API',
+  description: '練習串接 API。',
   link: 'superHeroSquad',
 }, {
   name: '身分證',
-  description: 'Hahow 動畫互動網頁程式入門課程練習',
+  description: 'Hahow 動畫互動網頁程式入門課程練習，使用 SCSS @mixin, @function 語法。',
   link: 'IDCard/dist',
 }, {
   name: 'CSS background 顏色選擇器',
-  description: '使用 overflow 與給定 height 值，讓頁面滾動到邊界彈回，看起來更具穩定性。實作 Tooltip，偵測滑鼠在元素的位置，並顯示。實作複製頁面內容到使用者剪貼板功能，方便操作。',
+  description: '列出所有 HTML 顏色名，方便選色。<br>使用 overflow 與給定 height 值，讓頁面滾動到邊界彈回，看起來更具穩定性。實作 Tooltip，偵測滑鼠在元素的位置，並顯示。實作複製頁面內容到使用者剪貼板功能，方便操作。',
   link: 'backgroundColorPicker/dist',
 }, {
   name: 'Read Clipboard',
-  description: '讀取使用者的 Clipboard 內容',
+  description: '使用 JavaScript 讀取使用者的 clipboard 內容。',
   link: 'readClipboard',
 }, {
   name: 'Todo List',
-  description: '用 JQuery 實作簡易備忘錄，並將資料儲存在 localStorage 予以保存資料，用來比較使用 Vue.js 後的優點',
+  description: '使用 JQuery 實作簡易備忘錄，將資料儲存在 localStorage，使網頁重載後不會消失。<br>用來比較使用 Vue.js 後的優點。',
   link: 'todoList/dist',
 }, {
-  name: 'sprite image with css animation',
-  description: '使用 sprite 圖片，搭配 CSS animation 播放動畫',
+  name: 'sprite image with CSS animation',
+  description: '使用 sprite 圖片，搭配 CSS animation 達到動畫圖片效果。',
   link: 'cssAnimation/dist',
 }];
 
